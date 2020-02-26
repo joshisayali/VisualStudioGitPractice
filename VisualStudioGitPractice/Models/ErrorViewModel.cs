2e2cdb05de417f521b0a3c618a24f1d79cb8d070
Sayali Joshi <sayalij@missionbell.com>
2020-02-25 20:15:16 -0800

    Add project files.

---
+++ VisualStudioGitPractice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VisualStudioGitPractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

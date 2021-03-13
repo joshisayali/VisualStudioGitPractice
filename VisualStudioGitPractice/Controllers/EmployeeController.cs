4346cc9333d2d34ee956a42843720236dc7b2fae
Sayali Joshi <sayalij@missionbell.com>
2021-03-12 16:37:59 -0800

    Added view Employee

---
+++ VisualStudioGitPractice/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VisualStudioGitPractice.Models;
+
+namespace VisualStudioGitPractice.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Employee emp = new Employee();
+            emp.Name = "John Doe";
+            return View(emp);
+        }
+    }
+}
---
+++ VisualStudioGitPractice/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisualStudioGitPractice.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Department { get; set; }
+        public string Location { get; set; }
+        public string Title { get; set; }
+
+    }
+}
